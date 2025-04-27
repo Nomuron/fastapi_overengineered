a3033e87c9c40de573296c6fe16cadc4e1bab370
Nomuron <Nomuron@gmail.com>
2025-04-27 19:21:15 +0200

    Added practice params and first libraries

---
+++ src/routes/practice.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+practice_router = APIRouter()
+
+class Item(BaseModel):
+    name: str
+    desc: str
+
+
+@practice_router.post("/{item_name}")
+def practice_path_endpoint(item_name: str):
+    return {"message": f"This is a practice endpoint for {item_name}"} 
+
+@practice_router.post("")
+def practice_query_endpoint(param1: int, param2: str):
+    return {"message": f"This is a practice endpoint for {param1} and {param2}"}
+
+@practice_router.post("/json/body")
+def practice_body_function(item: Item):
+    return item
+
+
